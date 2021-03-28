9ae66faee37bd3a932453867b09288ef1fb6ee6e
Gülten Deregözü <gulten.deregozu@gmail.com>
2021-03-28 16:33:56 +0300

    Start vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+import router from './router';
+import App from './layouts/App.vue';
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+const app = new Vue({
+    router,
+    el: '#app',
+    render: h => h(App)
+});
